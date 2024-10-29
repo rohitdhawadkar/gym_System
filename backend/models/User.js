c379053d2095e4645bcad5a40ff17497a45e1381
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-10-29 06:33:37 +0530

    added User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
 
-// Define User Schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   age: { type: Number, required: true },
   gender: { type: String },
-  trainer: { type: mongoose.Schema.Types.ObjectId, ref: 'Trainer' },  // FK to Trainer
-  workoutPlan: { type: mongoose.Schema.Types.ObjectId, ref: 'WorkoutPlan' },  // FK to WorkoutPlan
-  diet: { type: mongoose.Schema.Types.ObjectId, ref: 'Diet' }  // FK to Diet
+  trainer: { type: mongoose.Schema.Types.ObjectId, ref: "Trainer" },
+  workoutPlan: { type: mongoose.Schema.Types.ObjectId, ref: "WorkoutPlan" },
+  diet: { type: mongoose.Schema.Types.ObjectId, ref: "Diet" },
 });
 
-// Create and Export the User Model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
